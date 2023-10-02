aae6874b8ffbca94ea63f29141447aff266a7430
trtDigital <commercial@trtdigital.ma>
2023-10-03 00:50:37 +0100

    first commit

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,11 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <Link href="/">
+          <a><img className="block mx-auto align-center" src='https://www.trtdigital.ma/wp-content/uploads/2021/08/trtDigital-Logo-Maroc-light_V4.0.svg' height="60px" alt='Agence Web Développement e-Commerce SEO SEA Maroc' ></img></a>
+        </Link>
       <p className="text-2xl dark:text-white text-center">
-        <Link href="/">
+        <Link href="https://www.trtdigital.ma/">
           <a>{name}</a>
         </Link>
       </p>
--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Agence Web Développement e-Commerce SEO SEA Maroc';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Création de sites web, webmarketing, référencement naturel – SEO SEA & réseaux sociaux';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Réalisé par l’agence Web SEO SEA trtDigital';
 
   return {
     name,
